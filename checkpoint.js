17e3ed9a1af0f0752d77ffb3131dec9eea7b438f
LAPTOP-MP5E825G\Francisco <ftbr0011@gmail.com>
2021-05-28 14:41:14 -0300

    update first twelve

--- checkpoint.js
+++ checkpoint.js
@@ -5,12 +5,7 @@
 // métodos o construir determinadas funciones explicados más abajo. Pero todos los métodos ya implementados
 // en las homeworks no es necesario que los vuelvan a definir.
 
-const {
-  Queue,
-  Node,
-  LinkedList,
-  BinarySearchTree
-} = require('./DS.js');
+const { Queue, Node, LinkedList, BinarySearchTree } = require("./DS.js");
 
 // ----------------------
 
@@ -26,10 +21,14 @@ const {
 // Pista: utilizar el método Array.isArray() para determinar si algun elemento de array es un array anidado
 // [Para más información del método: https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Objetos_globales/Array/isArray]
 
-var countArray = function(array){
-
-}
-
+var countArray = function (array) {
+  let forReturn = 0;
+  for (i of array) {
+    if (Array.isArray(i)) forReturn += countArray(i);
+    else forReturn += i;
+  }
+  return forReturn;
+};
 
 // EJERCICIO 2
 // Secuencia inventada: f(n) = (f(n-1) + f(n-2) + f(n-3)) x 2
@@ -49,8 +48,19 @@ var countArray = function(array){
 // secuenciaHenry(6) // 524 ya que el elemento de la posición 6 es 524
 // Para números negativos de n debe devolver false
 function secuenciaHenry(array, n) {
-  // Tu código aca:
-
+  //limpiamos el array
+  for (let i = 0; i < array.length; i++) {
+    if (typeof array[i] === "string") array[i] = array[i].length;
+  }
+  // devolvemos el n si pertenece al array, y filtramos los negativos [condiciones stop]
+  if (n < 0) return false;
+  if (n < array.length - 1) return array[n];
+  //incrementamos el largo del array en 1 utilizando la funcion dada
+  let aL = array.length;
+  let resu = (array[aL - 3] + array[aL - 2] + array[aL - 1]) * 2;
+  array.push(resu);
+  //aplicamos recursion hasta que lleguemos a generar el n pedido y lo devolvemos
+  return secuenciaHenry(array, n);
 }
 
 // ---------------------
@@ -69,11 +79,17 @@ function secuenciaHenry(array, n) {
 //    lista.add(3);
 //    lista.size(); --> 3
 
-LinkedList.prototype.size = function(){
+LinkedList.prototype.size = function () {
   // Tu código aca:
-
-}
-
+  if (!this.head) return 0;
+  let node = this.head;
+  let forReturn = 1;
+  while (node.next) {
+    node = node.next;
+    forReturn++;
+  }
+  return forReturn;
+};
 
 // EJERCICIO 4
 // Implementar el método removeFromPos dentro del prototype de LinkedList que deberá remover un elemento de
@@ -89,10 +105,31 @@ LinkedList.prototype.size = function(){
 // Ejemplo 2:
 //    Suponiendo que se pide una posición inválida: removeFromPos(8) --> false
 
-LinkedList.prototype.removeFromPos = function(pos){
-  // Tu código aca:
-
-}
+LinkedList.prototype.removeFromPos = function (pos) {
+  // si pos es invalida devolvemos false
+  let size = this.size();
+  if (pos > size || pos < 0) return false;
+  // si tanto size como pos son 0 devolvemos null
+  if (size === 0 && pos === 0) return null;
+  //generamos la variable donde devolveremos el valor del nodo elminado
+  let forReturn;
+  let node = this.head;
+  let act = 0;
+  //nos movemos hasta el nodo anterior al que debe ser removido
+  while (act < pos - 1) {
+    node = node.next;
+    act++;
+  }
+  // si pos es 0 la condicion es especial por lo tanto lo resolvemos en esta linea
+  if (pos === 0) {
+    forReturn = node.value;
+    this.head = this.head.next;
+  } else {
+    forReturn = node.next.value;
+    node.next = node.next.next;
+  }
+  return forReturn;
+};
 
 // EJERCICIO 5
 // Implementar la función orderLinkedList que ordene los elementos de la lista pasada por parámetro
@@ -103,14 +140,24 @@ LinkedList.prototype.removeFromPos = function(pos){
 // IMPORTANTE: Pueden usar algun método de ordenamiento ya visto para tener un arreglo ordenado y a
 // partir de él construir la nueva lista ordenada
 
-var orderLinkedList = function(linkedList){
-  // Tu código aca:
-
-}
-
+var orderLinkedList = function (linkedList) {
+  //convertimos la lista a array
+  let array = [];
+  node = linkedList.head;
+  while (node) {
+    array.push(node.value);
+    node = node.next;
+  }
+  // la ordenamos usando el metodo sort
+  array = array.sort((a, b) => a - b);
+  //creamos la nueva lista
+  forReturn = new LinkedList();
+  while (array.length > 0) forReturn.add(array.shift());
+  //retornamos
+  return forReturn;
+};
 
 // ----------------------
-
 
 // ----- QUEUE -----
 
@@ -131,15 +178,40 @@ var orderLinkedList = function(linkedList){
 // - Tener un ticket que corresponda con el evento (prop event de ticket)
 // - Que no haya ingresado ya otra persona al evento con ese mismo número de ticket
 // Finalmente la función debe devolver un arreglo con todos los nombres de las personas que pudieron ingresar
-// Importante!: Aquellas personas que no cumplan con los requisitos para ingresar deben ser removidos de la cola 
+// Importante!: Aquellas personas que no cumplan con los requisitos para ingresar deben ser removidos de la cola
 
-var controlAcces = function(queue, event){
+var controlAcces = function (queue, event) {
   // Tu código aca:
-
-}
+  // creamos las variables que almacenan tickets y personas admitidas
+  let admitted = [];
+  let tickets = [];
+  //vaciamos la queue
+  while (queue.size() != 0) {
+    let person = queue.dequeue();
+    // revisamos si es mayor de edad y si esta en el evento correspondiente
+    if (person["age"] >= 18 && person["ticket"]["event"] === event) {
+      let ok = true;
+      //revisamos que el n de ticket no se haya usado antes
+      for (ticket of tickets) {
+        if (person["ticket"]["number"] === ticket) ok = false;
+      }
+      //si pasa las condiciones anteriores lo admitimos sumandolo a las listas
+      if (ok) {
+        admitted.push(person);
+        tickets.push(person["ticket"]["number"]);
+      }
+    }
+  }
+  //volvemos a llenar la queue con los asistentes y armamos el array de nombres para retornar
+  let forReturn = [];
+  for (person of admitted) {
+    forReturn.push(person["fullname"]);
+    queue.enqueue(person);
+  }
+  return forReturn;
+};
 
 // ---------------
-
 
 // ----- BST -----
 
@@ -157,13 +229,13 @@ var controlAcces = function(queue, event){
 //      \
 //       5
 
-var generateBST = function(array){
-
-}
-
+var generateBST = function (array) {
+  forReturn = new BinarySearchTree(array.shift());
+  for (i of array) forReturn.insert(i);
+  return forReturn;
+};
 
 // ---------------
-
 
 // Ejercicio 8
 // Dado un arreglo ordenado, encontrar el índice de un elemento específico pasado como parámetro
@@ -177,11 +249,9 @@ var generateBST = function(array){
 //    binarySearch(array, 2) --> Devolvería 1 ya que array[1] = 2
 //    [Donde 2 sería el número sobre el cuál queremos saber su posición en el array]
 
-
 var binarySearch = function (array, target) {
   // Tu código aca:
-
-}
+};
 
 // EJERCICIO 9
 // Ordená un arreglo de números usando un bubble sort pero con algunas particularidades.
@@ -220,11 +290,9 @@ var binarySearch = function (array, target) {
 // ]
 // (Siempre el ordenamiento es de menor a mayor sea cual fuera la propiedad indicada para el orden)
 
-
-var specialSort = function(array, firstOrd, secondOrd) {
+var specialSort = function (array, firstOrd, secondOrd) {
   // Tu código aca:
-
-}
+};
 
 // ----- Closures -----
 
@@ -244,11 +312,9 @@ var specialSort = function(array, firstOrd, secondOrd) {
 
 function closureGreeting(prefix) {
   // Tu código aca:
-
 }
 
 // -------------------
-
 
 // ----- EXTRA CREDIT -----
 
@@ -261,9 +327,7 @@ function closureGreeting(prefix) {
 //    const anagrams = allAnagrams('abc');
 //    console.log(anagrams); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
 
-var allAnagrams = function(string, array, index) {
-
-};
+var allAnagrams = function (string, array, index) {};
 
 module.exports = {
   countArray,
@@ -276,5 +340,5 @@ module.exports = {
   specialSort,
   closureGreeting,
   generateBST,
-  orderLinkedList
-}
+  orderLinkedList,
+};
